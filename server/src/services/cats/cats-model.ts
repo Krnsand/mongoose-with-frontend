d66e84d55c47b60c418baff3b0ac0077d4c23606
David Jensen <tsourdox@icloud.com>
2025-04-24 10:31:38 +0200

    define cat schema and model

---
+++ server/src/services/cats/cats-model.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from "mongoose";
+
+const catSchema = new Schema({
+  name: { type: String, required: true },
+  pattern: { type: String, required: true },
+  colors: [String],
+  age: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+export const CatModel = model("Cat", catSchema);
