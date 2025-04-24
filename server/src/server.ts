a6ccff4a687d7b6f72e2c09738a90c5d24279e4f
David Jensen <tsourdox@icloud.com>
2025-04-24 10:09:47 +0200

    setup express code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hello!");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? 7777;
+
+app.use(express.json());
+
+app.get("/api/cats", (req, res) => {
+  res.status(200).json("Cats...");
+});
+
+app.post("/api/cats", (req, res) => {
+  res.status(201).json("Created...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running: http://localhost:${port}`);
+});
