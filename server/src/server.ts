9c3f796af90253b2e8b267e73f6383f856d1da37
David Jensen <tsourdox@icloud.com>
2025-04-24 11:55:54 +0200

    add .env file and load db url from it

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import { errorHandler } from "./middlewares";
 import { catsRouter } from "./services/cats/cats-router";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 7777;
+const dbUrl = process.env.DATABASE_URL ?? "mongodb://localhost:27017/cats-db";
 
 app.use(express.json());
 app.use(catsRouter);
 app.use(errorHandler);
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/cats-db");
+  await mongoose.connect(dbUrl);
   app.listen(port, () => {
     console.log(`Server is running: http://localhost:${port}`);
   });
