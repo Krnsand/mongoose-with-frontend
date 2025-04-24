851965dd6a9d8a252baa115b2c1dce1398c920c3
David Jensen <tsourdox@icloud.com>
2025-04-24 10:37:33 +0200

    use cat model in endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import { CatModel } from "./services/cats/cats-model";
 
 const app = express();
 const port = process.env.PORT ?? 7777;
 
 app.use(express.json());
 
-app.get("/api/cats", (req, res) => {
-  res.status(200).json("Cats...");
+app.get("/api/cats", async (req, res) => {
+  const cats = await CatModel.find({});
+  res.status(200).json(cats);
 });
 
-app.post("/api/cats", (req, res) => {
-  res.status(201).json("Created...");
+app.post("/api/cats", async (req, res) => {
+  const cat = await CatModel.create(req.body);
+  res.status(201).json(cat);
 });
 
 async function main() {
